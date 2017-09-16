ac573bf42d764cac8b9f5e9f05933f74a47d90f9
miriambej <justme0112@hotmail.com>
2017-09-16 22:35:32 +1000

    create login Popup component

--- src/react-components/Navbar/LoginPopup.js
+++ src/react-components/Navbar/LoginPopup.js
@@ -4,9 +4,17 @@ import Popup from './Popup';
 class LoginPopup extends React.Component {
   render() {
     return (
-
+      <Popup {...this.props}>
+        <img src="/img/kitty.png"/>
+        <h1>Login to Join The Comnunity</h1>
+        <p>CodeHunt is a community to share and geek out about the latest code, podcast and news. Join us :)</p>
+        <button className="facebook-btn">Login with Facebook</button>
+        <p>We'll never post to Facebook without your permission.</p>
+      </Popup>
     );
   }
 }
 
 export default LoginPopup;
+
+//{...this.props} it is a shortcode to write something like status={this.props.status} hidePopup={this.props.hidePopup}
--- src/react-components/Navbar/Popup.js
+++ src/react-components/Navbar/Popup.js
@@ -8,8 +8,8 @@ class Popup extends React.Component {
 
           <img src="/img/close.png" onClick={this.props.hidePopup}/>
         </section>
-        <section className="popup-content">
-          <section>POPUP CONTENT SHOULD BE HERE</section>
+        <section className="popup-content login-popup">
+          <section>{this.props.children}</section>
         </section>
 
       </section>
@@ -30,4 +30,5 @@ class Popup extends React.Component {
 //we dont have the pop up come up straight away because when we first render the Navbar, the popupStatus is false, so whe you click on login, we execute the showPopup, which set the status to true, and then it pass that status to the Popup Component.
 //  this.props.status? equals true, renderPopupContent, otherwise false.
 //when we close the button, we run the hidePopup function that put the status to false and stop rending Popup Component
+//this.props.children will show everything we have between open and close tags of Popup(in this case, it will show the Popup under LoginPopup class.)
 export default Popup;
