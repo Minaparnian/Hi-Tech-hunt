cfae8d914f42b36b48dc2fc3cf3b8fb85fb2e326
Mina Parnian <minaparnian66@gmail.com>
2017-09-16 15:37:51 +1000

    refactor productItem

--- src/react-components/Product/ProductList.js
+++ src/react-components/Product/ProductList.js
@@ -1,58 +1,29 @@
 import React from 'react';
+import ProductItem from './ProductItem';
 
 class ProductList extends React.Component {
   render () {
     return (
       <ul className="product-list">
-        <li className="product-item">
-          <a className="upvote-button" href="#">
-            <span>
-              <i className="fa fa-sort-asc"></i>
-            </span>
-              {this.props.productList[0].upvote}
-          </a>
-          <img className="product-item-media" src={this.props.productList[0].media} />
-          <section className="product-item-info">
-            <a href="#">
-              <h2>{this.props.productList[0].name}</h2>
-            </a>
-            <p>{this.props.productList[0].description}</p>
-            <a href="#">
-              <img className="small-avatar" src={this.props.productList[0].maker.avatar}/>
-            </a>
-          </section>
-            <a className="product-item-link" href={this.props.productList[0].link}>
-              <span>
-                <i className="fa fa-external-link"></i>
-              </span>
-            </a>
-        </li>
+          <ProductItem
+            id = {this.props.productList[0].id}
+            name = {this.props.productList[0].name}
+            link = {this.props.productList[0].link}
+            media = {this.props.productList[0].media}
+            upvote = {this.props.productList[0].upvote}
+            description = {this.props.productList[0].description}
+            maker = {this.props.productList[0].maker}
+            />
 
-
-        <li className="product-item">
-          <a className="upvote-button" href="#">
-            <span>
-              <i className="fa fa-sort-asc"></i>
-            </span>
-              {this.props.productList[1].upvote}
-          </a>
-          <img className="product-item-media" src={this.props.productList[1].media} />
-          <section className="product-item-info">
-            <a href="#">
-              <h2>{this.props.productList[1].name}</h2>
-            </a>
-            <p>{this.props.productList[1].description}</p>
-            <a href="#">
-              <img className="small-avatar" src={this.props.productList[1].maker.avatar}/>
-            </a>
-          </section>
-            <a className="product-item-link" href={this.props.productList[1].link}>
-              <span>
-                <i className="fa fa-external-link"></i>
-              </span>
-            </a>
-        </li>
-
+          <ProductItem
+              id = {this.props.productList[1].id}
+              name = {this.props.productList[1].name}
+              link = {this.props.productList[1].link}
+              media = {this.props.productList[1].media}
+              upvote = {this.props.productList[1].upvote}
+              description = {this.props.productList[1].description}
+              maker = {this.props.productList[1].maker}
+              />
       </ul>
 
     );
