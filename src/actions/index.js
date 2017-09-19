4d05a929d38475f2d2f2bfb799fc912956040a3e
miriambej <justme0112@hotmail.com>
2017-09-19 19:00:34 +1000

    alt setup

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import alt from '../alt';
+import Firebase from 'firebase';
+
+class Actions {
+
+  login() {
+    return (dispatch) => {
+      var provider = new Firebase.auth.FacebookAuthProvider();
+      Firebase.auth().signInWithPopup(provider).then(function(result) {
+        var user = result.user;
+        dispatch(user);
+
+      }).catch(function(error) {
+        console.log('Failed!', error);
+      });
+    }
+  }
+}
+
+export default alt.createActions(Actions);
--- src/react-components/Navbar/LoginPopup.js
+++ src/react-components/Navbar/LoginPopup.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import Popup from './Popup';
-import Firebase from 'firebase';
+import Actions from '../../actions';
 
 class LoginPopup extends React.Component {
-  
+
   handleLogin = () => {
-      var provider = new Firebase.auth.FacebookAuthProvider();
-      provider.addScope('public_profile');
-
-      Firebase.auth().signInWithPopup(provider).then(function(result) {
-        var user = result.user;
-        console.log('Login successfully!', user);
-      }).catch(function(error) {
-        console.log('Failed!', error);
-      });
+      Actions.login();
+      this.props.hidePopup();
   };
-  render(){
-    return(
-     <Popup {...this.props} style="login-popup">
 
-       <img src="/img/kitty.png"/>
-       <h1>Login To Join The Community</h1>
-       <p>Codehunt is the Community to share geek out about the latest code, podcast and news. Join us :)</p>
-       <button className="facebook-btn" onClick={this.handleLogin}>Login with Facebook</button>
-       <p>We will never post to your facebook without permission</p>
-     </Popup>
+  render() {
+    return (
+      <Popup {...this.props} style="login-popup">
+        <img src="/img/kitty.png"/>
+        <h1>Login to Join The Community</h1>
+        <p>CodeHunt is a Community to share and geek out about the latest code, podcast and news. Join us :)</p>
+        <button className="facebook-btn" onClick={this.handleLogin}>Login with Facebook</button>
+        <p>We'll never post to Facebook without your permission.</p>
+      </Popup>
     );
   }
 }
